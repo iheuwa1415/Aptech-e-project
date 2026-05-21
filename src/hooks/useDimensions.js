8d7def5c3f2ac16deeb681aa79932feb17e23497
Elly0816 <elzoremmanuel@gmail.com>
2026-05-21 20:41:27 +0100

    feat: add hook to keep track of screen size for responsiveness

---
+++ src/hooks/useDimensions.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+export const useDimensions = () => {
+  const [windowSize, setWindowSize] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return { ...windowSize, isMobile: windowSize.width < 768 };
+};
