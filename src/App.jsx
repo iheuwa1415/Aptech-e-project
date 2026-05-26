ae2b611376a011d48ff7fb47f88fd51ed8ee0953
kyle <kylezaj@gmail.com>
2026-05-26 03:24:44 -0700

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router';
 import './App.css';
 import './global.css';
-/*  import Header from './components/Header/header';  */
+/* import Header from './components/Header/header';  */
 
+import Footer from './components/Footer/footer';
+// Notice the lowercase "p" in "./pages/..." below:
 import { HomePage } from './pages/Homepage/HomePage';
 import { Continents } from './pages/Continents/Continents';
 import { Gallery } from './pages/Gallery/Gallery';
 import { Saved } from './pages/Saved/Saved';
 import { About } from './pages/About/About';
 
-function App() {
+function AppContent() {
+  const location = useLocation();
+
+  // Checks if the user is exactly on the root home page path
+  const isHomePage = location.pathname === "/";
+
   return (
-    <BrowserRouter>
+    <>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/continents" element={<Continents />} />
@@ -20,6 +27,17 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/hello" element={<div>Hello World</div>} />
       </Routes>
+
+      {/* Your global footer switches layouts smoothly based on the page route! */}
+      <Footer isHomePage={isHomePage} />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
     </BrowserRouter>
   );
 }
